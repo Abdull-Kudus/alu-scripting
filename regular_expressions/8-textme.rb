adb2ecad23654cdd224c8aa0b72fca8dd1d895d0
Abdull-Kudus <a.zakariam@alustudent.com>
2025-09-05 01:50:24 +0200

    regex

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
-Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
-List = [Format[0].compact, Format[1].compact, Format[2].compact]
-puts List.join(',')
+input = ARGV[0]
+from  = input.scan(/\[from:(.*?)\]/).join
+to    = input.scan(/\[to:(.*?)\]/).join
+flags = input.scan(/\[flags:(.*?)\]/).join
+puts "#{from},#{to},#{flags}"
--- regular_expressions/README.md
+++ regular_expressions/README.md
@@ -1,1 +1,2 @@
-Frontend dev
+Frontend Dev
+
